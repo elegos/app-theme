90e45f44b4d08bf0ae328ddb638fd79f7765cc6d
Giacomo "Mr. Wolf" Furlan <elegos@fastwebnet.it>
2020-01-09 17:05:53 +0100

    MenuElement sub-elements

--- src/MenuBar/MenuElement/index.tsx
+++ src/MenuBar/MenuElement/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import classnames from 'classnames'
+import { MdKeyboardArrowRight } from 'react-icons/md'
 
 export interface MenuElementDef {
   name: string
   onClick?: (event: React.MouseEvent | React.KeyboardEvent) => void  
   subElements?: MenuElementDef[]
+  isChecked?: boolean
   isSeparator?: boolean
 }
 
@@ -15,6 +18,9 @@ interface MenuElementProps {
 const MenuElement: React.FunctionComponent<MenuElementProps> = (props: MenuElementProps) => {
   const { onClick: onOuterClick, element } = props
   const { isSeparator, onClick } = element
+  const [isOpen, setOpen] = useState<boolean>(false)
+  const [closeTimeout, setCloseTimeout] = useState<number>(-1)
+  const selfRef = useRef<HTMLDivElement>(null)
 
   const onClickWrapper = (event: React.MouseEvent) => {
     if (onOuterClick) {
@@ -26,13 +32,47 @@ const MenuElement: React.FunctionComponent<MenuElementProps> = (props: MenuEleme
     }
   }
 
+  const onMouseEnter = () => {
+    clearTimeout(closeTimeout)
+    if (isOpen) {
+      return
+    }
+    setOpen(true)
+  }
+
+  const onMouseLeave = (event: React.MouseEvent) => {
+    const relatedTarget: Element|null = event.relatedTarget as Element
+    const isStillSelf = relatedTarget
+      && relatedTarget.closest
+      && relatedTarget.closest('MenuElement') === selfRef.current
+
+    if (!isStillSelf) {
+      clearTimeout(closeTimeout)
+      setCloseTimeout(setTimeout(() => setOpen(false), 750))
+    }
+  }
+
   return isSeparator
     ? <hr className="Separator" />
     : <div
+      ref={selfRef}
+      data-name={element.name}
       className="MenuElement"
       onClick={onClickWrapper}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
-      {props.element.name}
+      <span className="ElementText">{element.name}</span>
+      {(element.subElements || []).length > 0 && <MdKeyboardArrowRight className="SubElementsCaret" />}
+      <div className={classnames('MenuElements', 'side', { open: isOpen })}>
+        {(element.subElements || []).map((element) => (
+          <MenuElement
+            key={element.name}
+            element={element}
+            onClick={() => alert('click!')}
+          />)
+        )}
+      </div>
     </div>
 }
 export default MenuElement
--- src/main.stories.tsx
+++ src/main.stories.tsx
@@ -5,7 +5,12 @@ export default { title: 'Demo' }
 export const Composed = (): React.ReactElement => <div>
   <MenuBar elements={[
     { name: 'File', elements: [
-      { name: 'New File' },
+      { name: 'New File...', subElements: [
+        { name: 'Blank' },
+        { name: 'From template' },
+        { name: 'Separator #1', isSeparator: true },
+        { name: 'Sub - submenu', subElements: [{ name: 'Whatever' }] }
+      ] },
       { name: 'New Window' },
       { name: 'Separator #1', isSeparator: true },
       { name: 'Open File...', onClick: () => alert('Open file') },
